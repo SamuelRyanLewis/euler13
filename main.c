decb6e3d154fcd4d358d0df33c397fc0c7447b4b
SamuelRyanLewis <srl8336@rit.edu>
2016-08-12 12:04:38 -0400

    Initial commit. Main stub created, makefile included, 50-digit numbers file included

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * Project Euler Problem 13
+ * Large Sum
+ * Work out the first ten digits of the sum of the following [see file]
+ * one-hundred 50-digit numbers.
+ * 
+ * main provides the basic logic and flow fo the program.
+ */
+int main(void) {
+
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+main : main.c
+	gcc -o main main.c -std=c99 -Wall -g
